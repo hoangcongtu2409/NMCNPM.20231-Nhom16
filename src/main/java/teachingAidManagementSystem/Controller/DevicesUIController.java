dc6b0d61629e7916e0ffb78c3270fd0c2e56c4c6
Your Name <you@example.com>
2023-12-27 23:59:22 +0700

    git config --global user.email "you@example.com" git config --global user.name "Your Name"

--- src/main/java/teachingAidManagementSystem/Controller/DevicesUIController.java
+++ src/main/java/teachingAidManagementSystem/Controller/DevicesUIController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import teachingAidManagementSystem.App;
+import teachingAidManagementSystem.Classes.Devices;
 
 import java.io.IOException;
 import java.net.URL;
