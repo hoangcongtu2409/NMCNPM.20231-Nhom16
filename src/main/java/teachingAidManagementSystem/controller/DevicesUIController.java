6414657d2c56ff7492f8e47c64646ee8567b1d52
Duckkk <trongduc2003ht@gmail.com>
2023-12-29 03:22:04 +0700

    git config --global user.email "you@example.com" git config --global user.name "Your Name"

--- src/main/java/teachingAidManagementSystem/controller/DevicesUIController.java
+++ src/main/java/teachingAidManagementSystem/controller/DevicesUIController.java
@@ -7,6 +7,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.effect.BoxBlur;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.util.Callback;
 import teachingAidManagementSystem.App;
 import teachingAidManagementSystem.classes.Devices;
@@ -17,27 +20,13 @@ import java.util.ResourceBundle;
 
 public class DevicesUIController implements Initializable {
     @FXML
-    public void switchToHome() throws IOException {
-        App.setRoot("homeUI");
-    }
+    private AnchorPane mainWindow;
     @FXML
-    public void switchToManage() throws IOException {
-        App.setRoot("manageUI");
-    }
+    private AnchorPane addAndEditWindow;
     @FXML
-    public void switchToDevices() throws IOException {
-        App.setRoot("devicesUI");
-    }
+    private HBox addButtons;
     @FXML
-    public void switchToClients() throws IOException {
-        App.setRoot("clientsUI");
-    }
-    @FXML
-    public void switchToProfile() throws IOException {
-        App.setRoot("profileUI");
-    }
-
-    //Điều khiển TableView
+    private HBox editButtons;
     @FXML
     private TableView<Devices> table;
     @FXML
@@ -66,8 +55,30 @@ public class DevicesUIController implements Initializable {
     private TextField usableTextField;
     @FXML
     private TextField brokenTextField;
-
     private ObservableList<Devices> devicesList;
+    private Devices device;
+
+    @FXML
+    public void switchToHome() throws IOException {
+        App.setRoot("homeUI");
+    }
+    @FXML
+    public void switchToManage() throws IOException {
+        App.setRoot("manageUI");
+    }
+    @FXML
+    public void switchToDevices() throws IOException {
+        App.setRoot("devicesUI");
+    }
+    @FXML
+    public void switchToClients() throws IOException {
+        App.setRoot("clientsUI");
+    }
+    @FXML
+    public void switchToProfile() throws IOException {
+        App.setRoot("profileUI");
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         devicesList = FXCollections.observableArrayList(
@@ -84,6 +95,7 @@ public class DevicesUIController implements Initializable {
         addButtonToTable();
     }
 
+    //Thêm các nút edit vào các hàng
     private void addButtonToTable() {
         Callback<TableColumn<Devices, Void>, TableCell<Devices, Void>> cellFactory = new Callback<TableColumn<Devices, Void>, TableCell<Devices, Void>>() {
             @Override
@@ -98,7 +110,11 @@ public class DevicesUIController implements Initializable {
                         } else {
                             //Chuyển sang giao diện giống add nhưng có sẵn thông tin của devices cần sửa
                             btn.setOnAction(e -> {
-
+                                TableRow<Devices> row = getTableRow();
+                                if (row != null) {
+                                    device = row.getItem();
+                                    openEditWindow();
+                                }
                             });
                             setGraphic(btn);
                         }
@@ -109,8 +125,47 @@ public class DevicesUIController implements Initializable {
         };
         editColumn.setCellFactory(cellFactory);
     }
-    //Cái này nên làm chuyển sang giao diện khác hoặc popup window để add vào
-    public void addButton (ActionEvent event) {
+
+    //Mở của sổ để thêm thiết bị
+    @FXML
+    public void openAddWindow() {
+        addAndEditWindow.setVisible(true);
+        BoxBlur blur = new BoxBlur(5, 5, 3);
+        mainWindow.setEffect(blur);
+        addButtons.setVisible(true);
+        editButtons.setVisible(false);
+        iconTextField.setText(null);
+        idTextField.setText(null);
+        nameTextField.setText(null);
+        amountTextField.setText(null);
+        usableTextField.setText(null);
+        brokenTextField.setText(null);
+    }
+
+    //Mở của sổ chỉnh sửa thiết bị
+    @FXML
+    public void openEditWindow() {
+        addAndEditWindow.setVisible(true);
+        BoxBlur blur = new BoxBlur(5, 5, 3);
+        mainWindow.setEffect(blur);
+        addButtons.setVisible(false);
+        editButtons.setVisible(true);
+        iconTextField.setText(device.getIconDevice());
+        idTextField.setText(device.getIdDevice());
+        nameTextField.setText(device.getNameDevice());
+        amountTextField.setText(String.valueOf(device.getAmountDevice()));
+        usableTextField.setText(String.valueOf(device.getUsableDevice()));
+        brokenTextField.setText(String.valueOf(device.getBrokenDevice()));
+    }
+
+    //Đóng Popup window
+    @FXML
+    public void closePopup() {
+        addAndEditWindow.setVisible(false);
+        mainWindow.setEffect(null);
+    }
+
+    public void addDevice (ActionEvent event) {
         Devices newDevice = new Devices();
         newDevice.setIconDevice(iconTextField.getText());
         newDevice.setIdDevice(idTextField.getText());
@@ -119,21 +174,26 @@ public class DevicesUIController implements Initializable {
         newDevice.setUsableDevice(Integer.parseInt(usableTextField.getText()));
         newDevice.setBrokenDevice(Integer.parseInt(brokenTextField.getText()));
         devicesList.add(newDevice);
+        closePopup();
     }
 
-    public void deleteButton (ActionEvent event) {
-        Devices selected = table.getSelectionModel().getSelectedItem();
-        devicesList.remove(selected);
+    public void deleteDevice(ActionEvent event) {
+        devicesList.remove(device);
+        closePopup();
     }
 
-    public void selectDevice (ActionEvent event) {
-        Devices selected = table.getSelectionModel().getSelectedItem();
-        iconTextField.setText(selected.getIconDevice());
-        idTextField.setText(selected.getIdDevice());
-        nameTextField.setText(selected.getNameDevice());
-        amountTextField.setText(String.valueOf(selected.getAmountDevice()));
-        usableTextField.setText(String.valueOf(selected.getUsableDevice()));
-        brokenTextField.setText(String.valueOf(selected.getBrokenDevice()));
+    //Lưu lại thay đổi thiết bị
+    public void applyChanges (ActionEvent event) {
+        Devices updateDevice = new Devices();
+        updateDevice.setIconDevice(iconTextField.getText());
+        updateDevice.setIdDevice(idTextField.getText());
+        updateDevice.setNameDevice(nameTextField.getText());
+        updateDevice.setAmountDevice(Integer.parseInt(amountTextField.getText()));
+        updateDevice.setUsableDevice(Integer.parseInt(usableTextField.getText()));
+        updateDevice.setBrokenDevice(Integer.parseInt(brokenTextField.getText()));
+        int i = 0;
+        if (devicesList.get(i).equals(device))
+            devicesList.set(i, updateDevice);
+        closePopup();
     }
-
 }
