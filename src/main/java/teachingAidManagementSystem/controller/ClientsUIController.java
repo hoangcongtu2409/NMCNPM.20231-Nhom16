da1f972101e90d0c992deb3f05532440a9d5d319
Duckkk <trongduc2003ht@gmail.com>
2024-01-05 08:32:07 +0700

    git config --global user.email "you@example.com" git config --global user.name "Your Name"

--- src/main/java/teachingAidManagementSystem/controller/ClientsUIController.java
+++ src/main/java/teachingAidManagementSystem/controller/ClientsUIController.java
@@ -1,14 +1,25 @@
 package teachingAidManagementSystem.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import teachingAidManagementSystem.App;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class ClientsUIController {
+public class ClientsUIController implements Initializable {
     @FXML
     private AnchorPane mainWindow;
+    @FXML
+    private VBox allClientsBox;
+    private List<HBox> tripleFrameList;
+    private List<AnchorPane> frameList;
 
     @FXML
     public void switchToHome() throws IOException {
@@ -29,5 +40,29 @@ public class ClientsUIController {
     @FXML
     public void switchToProfile() throws IOException {
         App.setRoot("profileUI");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    private void addFrame() {
+        int i = frameList.size();
+        if (i % 3 == 0) {
+            double h = allClientsBox.getPrefHeight();
+            h += 300;
+            allClientsBox.setPrefHeight(h);
+            HBox hBox = new HBox();
+            hBox.setPrefHeight(270);
+            hBox.setSpacing(46);
+            allClientsBox.getChildren().add(hBox);
+        }
+
+        int j = tripleFrameList.size();
+        HBox hBox = tripleFrameList.get(j + 1);
+        AnchorPane pane = new AnchorPane();
+        pane.setPrefWidth(316);
+        Label nameLabel = new Label();
     }
 }
